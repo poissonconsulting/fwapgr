fb7da73c42962bc286456f206841dcb213e175b5
Seb Dalgarno <seb@poissonconsulting.ca>
2020-06-03 19:47:37 -0700

    add wrapper function stream_network

--- R/wrappers.R
+++ R/wrappers.R
@@ -1,19 +1,22 @@
-#' #' FWA feature
-#' #'
-#' #' Get features from table.
-#' #'
-#' #' @inheritParams fwa_feature
-#' #' @param gnis_name A character string of the gnis_name.
-#' #' @return A sf object
-#' #' @export
-#' #' @examples
-#' #' fwa_stream_network('Sangan River')
-#' fwa_stream_network <- function(gnis_name, columns = NULL,
-#'                         bounds = NULL, geom_column = "geom", epsg = 4326){
+#' FWA feature
 #'
-#'   chk_string(gnis_name)
-#'   table <- add_schema("fwa_stream_networks_sp")
-#'   filter <- filter_gnis(gnis_name)
+#' Get features from table.
 #'
-#'   fwa_feature(table, filter, columns, bounds, geom_column, epsg)
-#' }
+#' @inheritParams fwa_feature
+#' @param gnis_name A character string of the gnis_name.
+#' @return A sf object
+#' @export
+#' @examples
+#' fwa_gnis_stream_network('Sangan River')
+fwa_gnis_stream_network <- function(gnis_name, columns = NULL,
+                        bounds = NULL, geom_column = "geom",
+                        epsg = 4326){
+
+  chk_string(gnis_name)
+  table <- "fwa_stream_networks_sp"
+  filter <- filter_gnis(gnis_name)
+
+  fwa_feature(table = table, filter = filter, columns = columns,
+              bounds = bounds, geom_column = geom_column,
+              epsg = epsg)
+}
---
+++ tests/testthat/test-wrappers.R
@@ -0,0 +1,9 @@
+test_that("wrapper functions work", {
+  ## fwa_stream_network
+  gnis <- "Sangan River"
+  x <- fwa_gnis_stream_network(gnis_name = gnis, columns = "gnis_name")
+  expect_is(x, "sf")
+  expect_identical(nrow(x), 21L)
+  expect_true(all(x$gnis_name == gnis))
+
+})
