c512dc113e5b23b168560955b886cc218cb67509
Seb Dalgarno <seb@poissonconsulting.ca>
2020-08-29 23:07:50 -0700

    move fwa_bbox tests

--- tests/testthat/test-api-v1.R
+++ tests/testthat/test-api-v1.R
@@ -1,6 +1,6 @@
 test_that("API V1 functions work", {
   ### fwa_bbox
-  x <- fwa_bbox("fwa_lakes_poly")
+  x <- fwa_bbox("fwa_lakes_poly", filter = "gnis_name_1 = 'Sardis Pond'")
   expect_identical(length(x), 4L)
   expect_true(all(is.numeric(x)))
   expect_is(x, "bbox")
