8c883c3eb025371019525a695e600bee968413d7
Seb Dalgarno <seb@poissonconsulting.ca>
2020-08-29 23:07:32 -0700

    change fwa_feature to fwa_collection

--- tests/testthat/test-collections.R
+++ tests/testthat/test-collections.R
@@ -1,5 +1,5 @@
 test_that("API collections works", {
-  collection_id <- "fwa_stream_networks_sp"
+  table <- "fwa_stream_networks_sp"
 
   ### fwa_feature
   columns <- c("blue_line_key", "gnis_name")
@@ -8,45 +8,39 @@ test_that("API collections works", {
   filter <- list(gnis_name = "Chilliwack Creek")
 
   # default vals
-  x <- fwa_feature(collection_id, filter = filter)
+  x <- fwa_collection(table, filter = filter)
   expect_true(all(x$gnis_name == "Chilliwack Creek"))
   expect_is(x, "sf")
 
-  # check transform
-  # x <- fwa_watershed(blk, epsg = 3005)
-  # expect_identical(attr(x$geometry, "crs")$input, "EPSG:3005")
-
   # check columns
-  x <- fwa_feature(collection_id, filter = filter, columns = columns)
+  x <- fwa_collection(table, filter = filter, columns = columns)
   expect_is(x, "sf")
   expect_identical(c(columns, "geometry"), names(x))
 
   # check filter and bounds
-  x <- fwa_feature(collection_id, filter = filter, bbox = bbox)
+  x <- fwa_collection(table, filter = filter, bbox = bbox)
   expect_is(x, "sf")
   expect_true(nrow(x) > 0)
   expect_true(all(x$gnis_name == "Chilliwack Creek"))
 
   # returns features within bounds of lake table
-  x <- fwa_feature("fwa_lakes_poly", bbox = bbox)
+  x <- fwa_collection("fwa_lakes_poly", bbox = bbox)
   expect_is(x, "sf")
   expect_identical(sort(unique(x$gnis_name_1)), c("Ryder Lake", "Sardis Pond"))
 
   # check filter  when outside of bounds
   filter <- list(gnis_name_1 = 'Sardis Pond')
-  x <- fwa_feature("fwa_lakes_poly", filter = filter, bbox = bbox)
+  x <- fwa_collection("fwa_lakes_poly", filter = filter, bbox = bbox)
   expect_is(x, "sf")
   expect_identical(nrow(x), 1L)
 
   # check other schema
-  x <- fwa_feature("wbdhu12", bbox = bbox, schema = "usgs")
+  x <- fwa_collection("wbdhu12", bbox = bbox, schema = "usgs")
   expect_is(x, "sf")
   expect_identical(nrow(x), 2L)
 
   # check bbox outside feature
   filter <- list(gnis_name = "Sangan River")
-  expect_error(fwa_feature(collection_id, filter = filter, bbox = bbox), class = "chk_error")
-
-
+  expect_error(fwa_collection(table, filter = filter, bbox = bbox), class = "chk_error")
 
 })
