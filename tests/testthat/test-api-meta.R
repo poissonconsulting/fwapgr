992af78fd6ece099d87f654421059106fffcaca4
Joe Thorley <joe@poissonconsulting.ca>
2020-10-26 16:55:43 -0700

    test api meta

--- tests/testthat/test-api-meta.R
+++ tests/testthat/test-api-meta.R
@@ -1,14 +1,28 @@
-with_mock_api({
-  test_that("api meta functions work", {
+test_that("fwa_list_tables", {
+  x <- fwa_list_tables()
+  expect_is(x, "tbl")
+  expect_identical(names(x), c("table", "schema", "collection_id", "extent"))
 
-    ## list layers
-    x <- fwa_list_tables()
-    expect_is(x, "tbl")
-    expect_identical(names(x), c("table", "schema", "collection_id", "extent"))
+  expect_true(all(
+  c("hybas_lev12_v1c", "wbdhu12", "fwa_approx_borders", "fwa_assessment_watersheds_poly",
+    "fwa_basins_poly", "fwa_bays_and_channels_poly", "fwa_bc_boundary",
+    "fwa_coastlines_sp", "fwa_glaciers_poly", "fwa_islands_poly",
+    "fwa_lakes_poly", "fwa_linear_boundaries_sp", "fwa_manmade_waterbodies_poly",
+    "fwa_named_streams", "fwa_named_watersheds_poly", "fwa_obstructions_sp",
+    "fwa_rivers_poly", "fwa_stream_networks_sp", "fwa_stream_networks_sp_deleteme",
+    "fwa_watershed_groups_poly", "fwa_watershed_groups_subdivided",
+    "fwa_watersheds_poly", "fwa_wetlands_poly", "gba_railway_tracks_sp",
+    "gba_transmission_lines_sp", "gns_geographical_names_sp", "transport_line",
+    "trim_cultural_lines", "trim_cultural_points", "trim_ebm_ocean",
+    "fiss_fish_obsrvtn_events_sp", "fiss_fish_obsrvtn_pnt_distinct",
+    "fiss_fish_obsrvtn_pnt_sp", "fiss_fish_obsrvtn_unmatched", "fiss_obstacles_pnt_sp",
+    "pscis_assessment_svw", "pscis_design_proposal_svw", "pscis_events_barrier",
+    "pscis_habitat_confirmation_svw", "pscis_points_all", "pscis_remediation_svw")
+  %in% x$table))
+})
 
-    ## list columns
-    x <- fwa_list_columns("fwa_stream_networks_sp")
-    expect_is(x, "tbl")
-    expect_identical(names(x), c("name", "type", "description"))
-  })
+test_that("fwa_list_columns", {
+  x <- fwa_list_columns("fwa_stream_networks_sp")
+  expect_is(x, "tbl")
+  expect_identical(names(x), c("name", "type", "description"))
 })
--- tests/testthat/test-collections.R
+++ tests/testthat/test-collections.R
@@ -1,53 +1,49 @@
-with_mock_api({
-  test_that("API collections works", {
-    table <- "fwa_stream_networks_sp"
+test_that("fwa_collection", {
+  table <- "fwa_stream_networks_sp"
 
-    ### fwa_collection
-    columns <- c("blue_line_key", "gnis_name")
-    bbox <- c(-122.01, 49.11, -121.86, 49.16)
-    columns <- c("blue_line_key", "gnis_name")
-    filter <- list(gnis_name = "Chilliwack Creek")
+  ### fwa_collection
+  columns <- c("blue_line_key", "gnis_name")
+  bbox <- c(-122.01, 49.11, -121.86, 49.16)
+  columns <- c("blue_line_key", "gnis_name")
+  filter <- list(gnis_name = "Chilliwack Creek")
 
-    # default vals
-    x <- fwa_collection(table, filter = filter)
-    expect_true(all(x$gnis_name == "Chilliwack Creek"))
-    expect_is(x, "sf")
+  # default vals
+  x <- fwa_collection(table, filter = filter)
+  expect_true(all(x$gnis_name == "Chilliwack Creek"))
+  expect_is(x, "sf")
 
-    # test wrapper
-    x <- fwa_collection("fwa_named_streams", filter = filter)
+  # test wrapper
+  x <- fwa_collection("fwa_named_streams", filter = filter)
 
-    # check columns
-    x <- fwa_collection(table, filter = filter, columns = columns)
-    expect_is(x, "sf")
-    expect_identical(c(columns, "geometry"), names(x))
+  # check columns
+  x <- fwa_collection(table, filter = filter, columns = columns)
+  expect_is(x, "sf")
+  expect_identical(c(columns, "geometry"), names(x))
 
-    # check filter and bounds
-    x <- fwa_collection(table, filter = filter, bbox = bbox)
-    expect_is(x, "sf")
-    expect_true(nrow(x) > 0)
-    expect_true(all(x$gnis_name == "Chilliwack Creek"))
+  # check filter and bounds
+  x <- fwa_collection(table, filter = filter, bbox = bbox)
+  expect_is(x, "sf")
+  expect_true(nrow(x) > 0)
+  expect_true(all(x$gnis_name == "Chilliwack Creek"))
 
-    # returns features within bounds of lake table
-    x <- fwa_collection("fwa_lakes_poly", bbox = bbox)
-    expect_is(x, "sf")
-    expect_identical(sort(unique(x$gnis_name_1)), c("Ryder Lake", "Sardis Pond"))
+  # returns features within bounds of lake table
+  x <- fwa_collection("fwa_lakes_poly", bbox = bbox)
+  expect_is(x, "sf")
+  expect_identical(sort(unique(x$gnis_name_1)), c("Ryder Lake", "Sardis Pond"))
 
-    # check filter  when outside of bounds
-    filter <- list(gnis_name_1 = 'Sardis Pond')
-    x <- fwa_collection("fwa_lakes_poly", filter = filter, bbox = bbox)
-    expect_is(x, "sf")
-    expect_identical(nrow(x), 1L)
+  # check filter  when outside of bounds
+  filter <- list(gnis_name_1 = 'Sardis Pond')
+  x <- fwa_collection("fwa_lakes_poly", filter = filter, bbox = bbox)
+  expect_is(x, "sf")
+  expect_identical(nrow(x), 1L)
 
-    # check other schema
-    x <- fwa_collection("wbdhu12", bbox = bbox, schema = "usgs")
-    expect_is(x, "sf")
-    expect_identical(nrow(x), 2L)
+  # check other schema
+  x <- fwa_collection("wbdhu12", bbox = bbox, schema = "usgs")
+  expect_is(x, "sf")
+  expect_identical(nrow(x), 2L)
 
-    # check bbox outside feature
-    filter <- list(gnis_name = "Sangan River")
-    expect_error(fwa_collection(table, filter = filter, bbox = bbox), class = "chk_error")
-
-  })
+  # check bbox outside feature
+  filter <- list(gnis_name = "Sangan River")
+  expect_error(fwa_collection(table, filter = filter, bbox = bbox), class = "chk_error")
 
 })
-
