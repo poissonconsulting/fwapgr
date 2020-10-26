b9588814241408483e6ffe8b9f8cca0fb475b0e2
Joe Thorley <joe@poissonconsulting.ca>
2020-10-26 14:59:11 -0700

    - fwa_search_gnis_streams() and fwa_list_gnis_streams() now take gnis_name = ".*" instead of NULL as default argument

--- R/gnis.R
+++ R/gnis.R
@@ -1,36 +1,36 @@
-#' FWA list named streams
+#' FWA List Named streams
 #'
 #' List information on named streams including gnis_name.
 #'
-#' @return A tibble
-#' @param gnis_name A regular expression to filter table by gnis_name. If Null (default), returns all rows.
+#' @param gnis_name A regular expression to filter table by gnis_name.
+#' By default returns all values.
 #' @param ignore_case A flag indicating whether to ignore case in regular expression.
+#' @return A tibble of the named streams, including stream order and watershed group code.
 #' @export
 #' @examples
 #' fwa_list_gnis_streams()
-fwa_list_gnis_streams <- function(gnis_name = NULL, ignore_case = TRUE){
-  if(is.null(gnis_name))
-    return(gnis_stream)
-
+fwa_list_gnis_streams <- function(gnis_name = ".*", ignore_case = TRUE){
+  chk_string(gnis_name)
+  chk_flag(ignore_case)
   i <- grep(gnis_name, gnis_stream$gnis_name, ignore.case = ignore_case)
   gnis_stream[i,]
 }
 
-#' FWA search named streams
+#' FWA Search Named streams
 #'
-#' Match valid stream gnis_name with regular expression.
+#' Get stream gnis_names that match with regular expression.
 #'
-#' @return A tibble
-#' @param gnis_name A regular expression to match gnis_name. If Null (default), returns all values.
+#' @param gnis_name A regular expression to match gnis_name.
+#' By default returns all values.
 #' @param ignore_case A flag indicating whether to ignore case in regular expression.
+#' @return A character vector of the stream names.
 #' @export
 #' @examples
 #' fwa_search_gnis_streams("sangan")
-fwa_search_gnis_streams <- function(gnis_name = NULL, ignore_case = TRUE){
+fwa_search_gnis_streams <- function(gnis_name = ".*", ignore_case = TRUE){
+  chk_string(gnis_name)
+  chk_flag(ignore_case)
   x <- unique(as.character(gnis_stream$gnis_name))
-  if(is.null(gnis_name))
-    return(x)
-
   i <- grep(gnis_name, x, ignore.case = ignore_case)
   x[i]
 }
