ff8ee2cce3820a0f0c0d975169e0e1f65cde1c91
Joe Thorley <joe@poissonconsulting.ca>
2020-10-26 17:00:55 -0700

    api utils do direct calls

--- tests/testthat/test-api-utils.R
+++ tests/testthat/test-api-utils.R
@@ -1,18 +1,16 @@
-with_mock_api({
-  test_that("api utils", {
-    path <- "fwapg/collections/whse_basemapping.fwa_stream_networks_sp/items.json"
-    query <- list(gnis_name = "Sangan River")
-    bad_path <- "fwapg/collections/fwa_stream_networks_sp/"
-    expect_error(fwa_api(path, list(1)), class = "chk_error")
-    expect_error(fwa_api(1, list(a = 1)), class = "chk_error")
-    expect_error(fwa_api(path, 1), class = "chk_error")
+test_that("api utils", {
+  path <- "fwapg/collections/whse_basemapping.fwa_stream_networks_sp/items.json"
+  query <- list(gnis_name = "Sangan River")
+  bad_path <- "fwapg/collections/fwa_stream_networks_sp/"
 
-    expect_error(fwa_api(bad_path, query), class = "chk_error")
+  expect_error(fwa_api(path, list(1)), class = "chk_error")
+  expect_error(fwa_api(1, list(a = 1)), class = "chk_error")
+  expect_error(fwa_api(path, 1), class = "chk_error")
+  expect_error(fwa_api(bad_path, query), class = "chk_error")
 
-    x <- fwa_api(path, query)
-    expect_is(x, "character")
+  x <- fwa_api(path, query)
+  expect_is(x, "character")
 
-    x <- read_feature(x, 4326)
-    expect_is(x, "sf")
-  })
+  x <- read_feature(x, 4326)
+  expect_is(x, "sf")
 })
