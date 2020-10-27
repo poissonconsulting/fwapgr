b32b76addca6ea1cc79be52fd0df19090b5d3873
Seb Dalgarno <seb.dalgarno@gmail.com>
2020-10-27 00:21:06 -0700

    - replaced fwa_list_tables with fwa_meta_collections and no longer separates table and schema from collection_id - replace fwa_list_columns with fwa_meta_properties and accepts collection_id not table and schema

--- R/metadata.R
+++ R/metadata.R
@@ -1,12 +1,12 @@
-#' FWA list tables
+#' FWA collections metadata
 #'
-#' List available tables in data collections.
+#' Provide collection metadata.
 #'
 #' @return A tibble
 #' @export
 #' @examples
-#' \dontrun{fwa_list_tables()}
-fwa_list_tables <- function(){
+#' \dontrun{fwa_meta_collections()}
+fwa_meta_collections <- function(){
 
   path <- "fwapg/collections"
   query <- list(a = NULL)
@@ -14,26 +14,23 @@ fwa_list_tables <- function(){
   resp <- fwa_api(path, query)
 
   df <- jsonlite::fromJSON(resp)$collections
-  df$table <- sapply(strsplit(df$id, "\\."), function(x) x[2])
-  df$schema <- sapply(strsplit(df$id, "\\."), function(x) x[1])
   df$collection_id <- df$id
-  df <- df[c("table", "schema", "collection_id", "extent")]
+  df <- df[c("collection_id", "description", "extent", "links")]
   tibble::as_tibble(df)
 }
 
-#' FWA list columns
+#' FWA property metadata
 #'
-#' List columns in a table.
+#' Provide column names of collection.
 #'
 #' @return A tibble
 #' @inheritParams fwa_collection
 #' @export
 #' @examples
-#' \dontrun{fwa_list_columns("fwa_stream_networks_sp")}
-fwa_list_columns <- function(table, schema = "whse_basemapping"){
+#' \dontrun{fwa_meta_properties("whse_basemapping.fwa_stream_networks_sp")}
+fwa_meta_properties <- function(collection_id){
 
-  collection <- add_schema(table, schema)
-  path <- glue("fwapg/collections/{collection}")
+  path <- glue("fwapg/collections/{collection_id}")
   query <- list(a = NULL)
 
   resp <- fwa_api(path, query)
--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -1,32 +1,58 @@
-test_that("fwa_list_tables", {
-  x <- fwa_list_tables()
+test_that("fwa_meta_collections", {
+  x <- fwa_meta_collections()
   expect_is(x, "tbl")
-  expect_identical(names(x), c("table", "schema", "collection_id", "extent"))
+  expect_identical(names(x), c("collection_id", "description", "extent", "links"))
 
   expect_true(all(
-    c("hybas_lev12_v1c", "wbdhu12", "fwa_approx_borders", "fwa_assessment_watersheds_poly",
-      "fwa_basins_poly", "fwa_bays_and_channels_poly", "fwa_bc_boundary",
-      "fwa_coastlines_sp", "fwa_glaciers_poly", "fwa_islands_poly",
-      "fwa_lakes_poly", "fwa_linear_boundaries_sp", "fwa_manmade_waterbodies_poly",
-      "fwa_named_streams", "fwa_named_watersheds_poly", "fwa_obstructions_sp",
-      "fwa_rivers_poly", "fwa_stream_networks_sp", "fwa_stream_networks_sp_deleteme",
-      "fwa_watershed_groups_poly", "fwa_watershed_groups_subdivided",
-      "fwa_watersheds_poly", "fwa_wetlands_poly", "gba_railway_tracks_sp",
-      "gba_transmission_lines_sp", "gns_geographical_names_sp", "transport_line",
-      "trim_cultural_lines", "trim_cultural_points", "trim_ebm_ocean",
-      "fiss_fish_obsrvtn_events_sp", "fiss_fish_obsrvtn_pnt_distinct",
-      "fiss_fish_obsrvtn_pnt_sp", "fiss_fish_obsrvtn_unmatched", "fiss_obstacles_pnt_sp",
-      "pscis_assessment_svw", "pscis_design_proposal_svw", "pscis_events_barrier",
-      "pscis_habitat_confirmation_svw", "pscis_points_all", "pscis_remediation_svw")
-    %in% x$table))
+    c("hydrosheds.hybas_lev12_v1c",
+      "usgs.wbdhu12",
+      "whse_basemapping.fwa_approx_borders",
+      "whse_basemapping.fwa_assessment_watersheds_poly",
+      "whse_basemapping.fwa_basins_poly",
+      "whse_basemapping.fwa_bays_and_channels_poly",
+      "whse_basemapping.fwa_bc_boundary",
+      "whse_basemapping.fwa_coastlines_sp",
+      "whse_basemapping.fwa_glaciers_poly",
+      "whse_basemapping.fwa_islands_poly",
+      "whse_basemapping.fwa_lakes_poly",
+      "whse_basemapping.fwa_linear_boundaries_sp",
+      "whse_basemapping.fwa_manmade_waterbodies_poly",
+      "whse_basemapping.fwa_named_streams",
+      "whse_basemapping.fwa_named_watersheds_poly",
+      "whse_basemapping.fwa_obstructions_sp",
+      "whse_basemapping.fwa_rivers_poly",
+      "whse_basemapping.fwa_stream_networks_sp",
+      "whse_basemapping.fwa_stream_networks_sp_deleteme",
+      "whse_basemapping.fwa_watershed_groups_poly",
+      "whse_basemapping.fwa_watershed_groups_subdivided",
+      "whse_basemapping.fwa_watersheds_poly",
+      "whse_basemapping.fwa_wetlands_poly",
+      "whse_basemapping.gba_railway_tracks_sp",
+      "whse_basemapping.gba_transmission_lines_sp",
+      "whse_basemapping.gns_geographical_names_sp",
+      "whse_basemapping.transport_line",
+      "whse_basemapping.trim_cultural_lines",
+      "whse_basemapping.trim_cultural_points",
+      "whse_basemapping.trim_ebm_ocean",
+      "whse_fish.fiss_fish_obsrvtn_events_sp",
+      "whse_fish.fiss_fish_obsrvtn_pnt_distinct",
+      "whse_fish.fiss_fish_obsrvtn_pnt_sp",
+      "whse_fish.fiss_fish_obsrvtn_unmatched",
+      "whse_fish.fiss_obstacles_pnt_sp",
+      "whse_fish.pscis_assessment_svw",
+      "whse_fish.pscis_design_proposal_svw",
+      "whse_fish.pscis_events_barrier",
+      "whse_fish.pscis_habitat_confirmation_svw",
+      "whse_fish.pscis_points_all",
+      "whse_fish.pscis_remediation_svw")
+    %in% x$collection_id))
 
-  expect_true(all(
-    c("hydrosheds", "usgs", "whse_basemapping", "whse_fish")
-    %in% x$schema))
+
+
 })
 
-test_that("fwa_list_columns", {
-  x <- fwa_list_columns("fwa_stream_networks_sp")
+test_that("fwa_meta_properties", {
+  x <- fwa_meta_properties("whse_basemapping.fwa_stream_networks_sp")
   expect_is(x, "tbl")
   expect_identical(names(x), c("name", "type", "description"))
 })
