2432483e51244ea43b8d8db04ca54158177715d2
Joe Thorley <joe@poissonconsulting.ca>
2020-10-28 18:12:02 -0700

    collections as href

--- R/collections.R
+++ R/collections.R
@@ -14,7 +14,7 @@
 #' @param transform A character string of the geometry transformation function pipeline to apply.
 #' @param epsg A positive whole number of the epsg to transform features to.
 #' @return A sf object
-#' @seealso \url{https://www.hillcrestgeo.ca/fwapg/collections.html} for API documentation.
+#' @seealso \href{https://www.hillcrestgeo.ca/fwapg/collections.html}{collections API documentation}.
 #' @export
 #' @examples
 #' \dontrun{
