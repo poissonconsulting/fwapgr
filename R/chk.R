56947a38e4330f0c47bbfe87a01848ef5b83ac3b
Joe Thorley <joe@poissonconsulting.ca>
2021-09-04 07:19:15 -0700

    chk return invisible object

--- R/chk.R
+++ R/chk.R
@@ -1,41 +1,45 @@
-chk_response_status <- function(x){
+chk_response_status <- function(x, x_name = NULL){
   if(vld_response_status(x)) {
-    return(invisible())
+    return(invisible(x))
   }
   msg <- status_msg(x)
   abort_chk(glue("API request failed [{status_code(x)}]: {msg}"))
 }
 
-chk_response_json <- function(x){
+chk_response_json <- function(x, x_name = NULL){
   if (vld_response_json(x)){
-    return(invisible())
+    return(invisible(x))
   }
   msg <- status_msg(x)
+  if (is.null(x_name)) x_name <- deparse_backtick_chk((substitute(x)))
   abort_chk(glue("API did not return JSON: {msg}"))
 }
 
-chk_response_gateway <- function(x){
+chk_response_gateway <- function(x, x_name = NULL){
   if (vld_response_gateway(x)){
-    return(invisible())
+    return(invisible(x))
   }
   msg <- "Bad Gateway"
+  if (is.null(x_name)) x_name <- deparse_backtick_chk((substitute(x)))
   abort_chk(glue("API request failed [{status_code(x)}]: {msg}"))
 }
 
-chk_bbox <- function(x){
+chk_bbox <- function(x, x_name = NULL){
   if(vld_bbox(x)) {
-    return(invisible())
+    return(invisible(x))
   }
-  chk_numeric(x)
-  chk_length(x, 4L)
-  chk_not_any_na(x)
+  if (is.null(x_name)) x_name <- deparse_backtick_chk((substitute(x)))
+  chk_numeric(x, x_name = x_name)
+  chk_length(x, 4L, x_name = x_name)
+  chk_not_any_na(x, x_name = x_name)
 }
 
-chk_transform <- function(x){
+chk_transform <- function(x, x_name = NULL){
   if(vld_transform(x)) {
-    return(invisible())
+    return(invisible(x))
   }
-  chk_character(x)
-  chk_not_any_na(x)
-  chk_subset(x[1], valid_transform_functions())
+  if (is.null(x_name)) x_name <- deparse_backtick_chk((substitute(x)))
+  chk_character(x, x_name = x_name)
+  chk_not_any_na(x, x_name = x_name)
+  chk_subset(x[1], valid_transform_functions(), x_name = x_name)
 }
