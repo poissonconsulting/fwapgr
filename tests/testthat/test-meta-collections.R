eb0d2b0cb2fc4e589692bd7fb86280a8fb2dad4d
Joe Thorley <joe@poissonconsulting.ca>
2021-09-04 11:18:43 -0700

    snapshot for collections

--- tests/testthat/test-meta-collections.R
+++ tests/testthat/test-meta-collections.R
@@ -1,6 +1,7 @@
-test_that("fwa_meta_collections are recognised", {
+test_that("fwa_meta_collections works the values haven't changed", {
   collections <- fwa_meta_collections()
   expect_s3_class(collections, "tbl")
   expect_identical(names(collections), c("collection_id", "description", "extent", "links"))
-  expect_setequal(collections$collection_id, collection_ids())
+  expect_snapshot_data(collections[c("collection_id", "description")], "collections")
 })
+
--- tests/testthat/test-meta-properties.R
+++ tests/testthat/test-meta-properties.R
@@ -1,4 +1,4 @@
-test_that("fwa_meta_properties works", {
+test_that("fwa_meta_properties works for stream networks", {
   properties <- fwa_meta_properties("whse_basemapping.fwa_stream_networks_sp")
   expect_s3_class(properties, "tbl_df")
   expect_snapshot_data(properties, "properties")
