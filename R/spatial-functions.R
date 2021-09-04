a6e220d638dd9018fbeab3fb208ef961d41cc70b
Joe Thorley <joe@poissonconsulting.ca>
2021-09-04 09:26:20 -0700

    doc watershed at downstream route measure

--- R/spatial-functions.R
+++ R/spatial-functions.R
@@ -1,9 +1,9 @@
 #' FWA Index Point
 #'
-#' Gets the nearest point on a stream to the coordinates within the tolerance.
+#' Gets the nearest point on a stream to the coordinates (within the tolerance).
 #'
-#' To return the first and second closest points on streams within the tolerance
-#' increase limit to 2.
+#' To also return the second nearest point to a stream (within the tolerance)
+#' set `limit = 2`.
 #'
 #' @inherit internal
 #' @family functions
@@ -20,12 +20,12 @@ fwa_index_point <- function(x, y, srid = 4326,
                             epsg = 4326) {
   chk_number(x)
   chk_number(y)
+  chk_whole_number(srid)
+  chk_gt(srid)
   chk_number(tolerance)
   chk_gt(tolerance)
   chk_whole_number(limit)
   chk_gt(limit)
-  chk_whole_number(srid)
-  chk_gt(srid)
 
   parameters <- list(
     x = x,
@@ -49,9 +49,31 @@ fwa_index_point <- function(x, y, srid = 4326,
 #'
 #' Gets a polygon of the watershed at a specified river metre.
 #'
+#' The function selects and aggregate all the fundamental watershed polygons
+#' upstream of the fundamental watershed in which the point lies.
+#'
+#' Depending on where the point is in the fundamental watershed one of four
+#' things can happen to the fundamental watershed.
+#'
+#' 1. If the point is < 50 m upstream from the bottom of the fundamental
+#' watershed then the fundamental watershed is included in the aggregation
+#' (recorded as KEEP in the refine_method property).
+#'
+#' 2. Else if the point is < 100m downstream from the top of the fundamental
+#' watershed then the fundamental watershed is not included in the aggregation
+#' (recorded as DROP in the refine_method property).
+#'
+#' 3. Else if the point is on a polygonal river/canal the fundamental watershed
+#' is cut across the banks of the river before being included in the aggregation
+#' (recorded as CUT in the refine_method property).
+#'
+#' 4. Otherwise something else happens (recorded as DEM in the refine_method
+#' property).
+#'
 #' @inherit internal
 #' @family functions
 #' @seealso [API documentation](https://www.hillcrestgeo.ca/fwapg/functions/fwa_watershedatmeasure.html)
+#'
 #' @export
 #' @examples
 #' fwa_watershed_at_measure(356308001, downstream_route_measure = 10000)
